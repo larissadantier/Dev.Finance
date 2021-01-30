2fc7ad0927839a457c0645f5898b76dd09068560
unknown <larissa_dantier@hotmail.com>
2021-01-30 18:54:06 -0300

    Script - income and expense

--- script.js
+++ script.js
@@ -33,13 +33,27 @@ const transactions = [{
 
 const Transaction = {
     incomes(){
-        // Somar as entradas
-        
+        let income = 0;
+        transactions.forEach((transaction) => {
+            if(transaction.amount > 0){
+                income += transaction.amount;
+            }
+        })
+
+        return income
     },
     expenses(){
-        // Somar as saídas
+        let expense = 0;
+        transactions.forEach((transaction) => {
+            if(transaction.amount < 0){
+                expense += transaction.amount;
+            }
+        })
+
+        return expense
     },
     total(){
+        return "Discover"
         // Entradas - Saídas
     }
 };
@@ -59,20 +73,48 @@ const DOM =  {
 
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td><img src="./assets/minus.svg" alt="Remover transação"></td>
         `
         return html
+    },
+    updateBalance(){
+        document.
+        getElementById('incomeDisplay')
+        .innerHTML = Transaction.incomes();
+
+        document.
+        getElementById('expenseDisplay')
+        .innerHTML = Transaction.expenses();
+
+        document.
+        getElementById('totalDisplay')
+        .innerHTML = Transaction.total()
     }
 }
 
 const Utils = {
     formatCurrency(value){
         const signal = Number(value) < 0 ? "-" : "";
+
+        value = String(value).replace(/\D/g,"");
+
+        value = Number(value) / 100;
+
+        value = value.toLocaleString("pt-BR", {
+            style:"currency",
+            currency: "BRL"
+        });
+
+        return signal + value
     }
 }
+
+
 
 transactions.forEach(function(transaction) {
     DOM. addTransaction(transaction);
 })
+
+DOM.updateBalance()
